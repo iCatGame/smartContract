511f359a0d31be6bac29af781e6c3e06f6686656
kongtaoxing <xyf8658@163.com>
2023-07-05 23:36:18 +0800

    add etherscan verify

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,24 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-etherscan");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+    networks: {
+      arbitrumGoerli: {
+        url: "https://goerli-rollup.arbitrum.io/rpc",
+        accounts: []
+      },
+    },
+    etherscan: {
+      apiKey: "I1PKX2ATV2VK4QW2US6FEEEVDF1XWW2AM6",
+  },
   solidity: {
     version: "0.8.17",
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200,
+        runs: 400,
       },
     },
   },
-  
 };
